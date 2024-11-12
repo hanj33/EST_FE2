99072c27931b89469f6128fc6b0d82245a6f9d76
han.j33 <jeehyun0212@naver.com>
2024-11-12 13:05:22 +0900

    241112 오전

---
+++ 241112/TDD/JasmineTDD/textManager.spec.js
@@ -0,0 +1,10 @@
+describe("텍스트 관리자 테스트 모음입니다", () => {
+  const textManager = new TextManager();
+
+  it("관리하고 있는 텍스트 값을 반환합니다", () => {
+    const initValue = textManager.getValue();
+
+    expect();
+  });
+  it("관리하고 있는 텍스트 값을 수정합니다", () => {});
+});
